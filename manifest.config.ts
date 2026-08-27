84bfa4f1fd30b81f03daa24fadd63d00fbee399c
namahu <neti0326@gmail.com>
2026-08-27 17:37:01 +0900

    chore: Generating and applying icon images

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,11 +6,17 @@ export default defineManifest({
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    16: 'public/icon-16.png',
+    32: 'public/icon-32.png',
+    48: 'public/icon-48.png',
+    128: 'public/icon-128.png',
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      16: 'public/icon-16.png',
+      32: 'public/icon-32.png',
+      48: 'public/icon-48.png',
+      128: 'public/icon-128.png',
     },
     default_popup: 'src/popup/index.html',
   },
